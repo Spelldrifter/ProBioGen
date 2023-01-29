dbdd5432998e39dc8d617bafe5bfb0aead106174
Spelldrifter <spelldrifter@proton.me>
2023-01-29 06:30:18 +0000

    fix type reference errors

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,1 +1,6 @@
-import { getOpenAIApiIns
+import { getOpenAIApiInstance } from "@/libs/OpenAiCompletaions"
+import { errorHandler } from "@/libs/utils";
+
+// export const dynamic = 'force-dynamic'
+
+const ap
