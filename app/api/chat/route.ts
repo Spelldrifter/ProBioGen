43c9054a779ddbb12134cfd13bcbb047d6dacf2d
Spelldrifter <spelldrifter@proton.me>
2023-01-29 16:23:04 +0000

    change route

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -3,4 +3,7 @@ import { errorHandler } from "@/libs/utils";
 
 // export const dynamic = 'force-dynamic'
 
-const ap
+const apikey = process.env.NEXT_PUBLIC_OPENAI_API_KEY
+
+export async function POST(request: Request) {
+  c
