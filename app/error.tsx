b97fb0e680237b15e6c9b08620bbf7fae219474b
Spelldrifter <spelldrifter@proton.me>
2023-02-05 20:57:33 +0000

    Update bezier.html

--- app/error.tsx
+++ app/error.tsx
@@ -1,3 +1,10 @@
 'use client' // Error components must be Client Components
 
-import { useEffect } from
+import { useEffect } from 'react'
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string }
+  reset: (
