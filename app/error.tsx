8980a5fac6e2becf35a7e9ec35f68e6e99453f93
Spelldrifter <spelldrifter@proton.me>
2023-02-08 13:36:25 +0000

    Fix the Travis configuration

--- app/error.tsx
+++ app/error.tsx
@@ -17,4 +17,5 @@ export default function Error({
   return (
     <section className="w-full flex text-center md:text-left justify-center">
       <div className=" flex flex-col py-10 md:py-44 items-center  gap-8 md:gap-24 w-full max-w-screen-xl mx-5 md:mx-20">
-        <h2 className='md:
+        <h2 className='md:text-xl text-primaryColor font-bold'>یه مشکلی به وجود اومده ⚠️</h2>
+        <span className='md:text-xl text-
