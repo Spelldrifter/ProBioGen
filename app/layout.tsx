da14b38fb398ed1f6b0c405af925a6eeaa294497
Spelldrifter <spelldrifter@proton.me>
2023-02-17 11:47:18 +0000

    Remove ignored files

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import Header from '@/components/Header';
 import '@/styles/globals.css';
 import Footer from '@/components/Footer';
-import 
+import { Toaster } from '@/components/ui/sonner';
+import { Metadata } from 'next';
+import { Analytics } from "@vercel/analytics
