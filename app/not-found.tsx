f305ef88c86e2ff3cdec08836d73e3b6b868440a
Spelldrifter <spelldrifter@proton.me>
2023-02-21 07:31:53 +0000

    Small Changes

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,4 +2,6 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div className="flex mx-auto my-10 items-cent
+    <div className="flex mx-auto my-10 items-center justify-center">
+      <div className=" text-center">
+        <h1 className="text-9xl font-black text-gray-200">404<
