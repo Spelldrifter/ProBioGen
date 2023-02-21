2e9ff53fe91c488a80148b25194a517df058a1f2
Spelldrifter <spelldrifter@proton.me>
2023-02-21 08:48:04 +0000

    I18n: Update translation nl (100%).

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,4 +4,10 @@ export default function NotFound() {
   return (
     <div className="flex mx-auto my-10 items-center justify-center">
       <div className=" text-center">
-        <h1 className="text-9xl font-black text-gray-200">404<
+        <h1 className="text-9xl font-black text-gray-200">404</h1>
+
+        <p className="text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+          اوه، نه!
+        </p>
+
+   
