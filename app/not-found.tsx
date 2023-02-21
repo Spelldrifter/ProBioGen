01e227689cf84dd4b48b8c31a266976024dd946b
Spelldrifter <spelldrifter@proton.me>
2023-02-21 11:36:52 +0000

    small mod

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -16,4 +16,4 @@ export default function NotFound() {
 
         <Link
           href="/"
-          classNam
+          className="mt-6 inline-block rounded bg-indigo-600 px-5 py-3 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-n
