25e2ed3f8dffcf74e20149226e7c947f08cb1669
Spelldrifter <spelldrifter@proton.me>
2023-03-02 08:37:24 +0000

    Update ice submodule to latest

--- components/LoadingDots.tsx
+++ components/LoadingDots.tsx
@@ -1,4 +1,11 @@
 import React from "react";
 import styles from "../styles/loading-dots.module.css";
 
-c
+const LoadingDots = ({
+  color = "#000",
+  style,
+}: {
+  color: string;
+  style?: string;
+}) => {
+  const loadingSty
