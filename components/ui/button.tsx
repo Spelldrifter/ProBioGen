572d7047ec785d9144d1834194649d367044f8d0
Spelldrifter <spelldrifter@proton.me>
2023-03-11 17:21:24 +0000

    Lint cleanup

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -4,4 +4,5 @@ import { cva, type VariantProps } from "class-variance-authority"
 
 import { cn } from "@/libs/utils"
 
-const buttonVariant
+const buttonVariants = cva(
+  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medi
