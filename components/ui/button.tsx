e876ca0113237d4b98606ead92d81f4b4b34c2d7
Spelldrifter <spelldrifter@proton.me>
2023-03-19 10:20:03 +0000

    Bump version to 2.7.0

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -5,4 +5,4 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/libs/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medi
+  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visib
