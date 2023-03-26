e8d4db1c7f8d9fff63ce03c644b13ea5850554da
Spelldrifter <spelldrifter@proton.me>
2023-03-26 18:38:33 +0000

    bower.json

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -11,4 +11,6 @@ const buttonVariants = cva(
       variant: {
         default: "bg-primary text-primary-foreground hover:bg-primary/90",
         destructive:
-          "bg-destructive text-des
+          "bg-destructive text-destructive-foreground hover:bg-destructive/90",
+        outline:
+          "border border-input bg-background hover:bg-
