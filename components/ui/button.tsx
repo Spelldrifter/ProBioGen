5f7d46f89254cda04fd439e96e68d02ea1396aee
Spelldrifter <spelldrifter@proton.me>
2023-03-30 12:57:03 +0000

    NFS: Fix a problem with the legacy binary mount code

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -16,4 +16,6 @@ const buttonVariants = cva(
           "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        gh
+        ghost: "hover:bg-accent hover:text-accent-foreground",
+        link: "text-primary underline-offset-4 hover:underline",
+  
