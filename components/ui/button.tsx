e5f65053f4f0f39a72cedae7dd1aa4b97415399f
Spelldrifter <spelldrifter@proton.me>
2023-04-02 09:32:44 +0000

    README: fix typo

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -23,3 +23,14 @@ const buttonVariants = cva(
         default: "h-10 px-4 py-2",
         sm: "h-9 rounded-md px-3",
         lg: "h-11 rounded-md px-8",
+        icon: "h-10 w-10",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    },
+  }
+)
+
+export i
