0437e9942bb7bf2d0326e03e9fb16e0ea8e78bf1
Spelldrifter <spelldrifter@proton.me>
2023-05-06 09:59:55 +0000

    Updated strings from Crowdin

--- components/ui/sonner.tsx
+++ components/ui/sonner.tsx
@@ -18,4 +18,6 @@ const Toaster = ({ ...props }: ToasterProps) => {
             "group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg",
           description: "group-[.toast]:text-muted-foreground",
           actionButton:
-      
+            "group-[.toast]:bg-primary group-[.toast]:text-primary-foreground",
+          cancelButton:
+            "group-[.toas
