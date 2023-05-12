6d496fd4d468ac8c52134db464a53adf8f23e76a
Spelldrifter <spelldrifter@proton.me>
2023-05-12 22:51:02 +0000

    Update Handlebars.Net.nuspec [ci skip]

--- components/ui/sonner.tsx
+++ components/ui/sonner.tsx
@@ -20,4 +20,12 @@ const Toaster = ({ ...props }: ToasterProps) => {
           actionButton:
             "group-[.toast]:bg-primary group-[.toast]:text-primary-foreground",
           cancelButton:
-            "group-[.toas
+            "group-[.toast]:bg-muted group-[.toast]:text-muted-foreground",
+        },
+      }}
+      {...props}
+    />
+  )
+}
+
+export { Toaster }
