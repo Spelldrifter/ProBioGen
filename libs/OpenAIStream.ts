846f1fa7ed26f610197143f7f74f2edf4c5c7bbe
Spelldrifter <spelldrifter@proton.me>
2023-10-17 07:36:25 +0000

    Version fix

--- libs/OpenAIStream.ts
+++ libs/OpenAIStream.ts
@@ -68,4 +68,7 @@ export async function OpenAIStream(payload: OpenAIStreamPayload) {
 
       // stream response (SSE) from OpenAI may be fragmented into multiple chunks
       // this ensures we properly read chunks and invoke an event for each SSE event stream
-      const parser = cr
+      const parser = createParser(onParse);
+      for await (const chunk of res.data as any) {
+        parser.feed(decoder.decode(chunk));
+      }
