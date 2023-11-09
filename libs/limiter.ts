c07e09e4376e15e9bec8825650c642c53284ff3f
Spelldrifter <spelldrifter@proton.me>
2023-11-09 21:56:56 +0000

    code cleaned

--- libs/limiter.ts
+++ libs/limiter.ts
@@ -1,2 +1,9 @@
 import { RateLimitError } from "./errors";
-import { getUserIp } from "./ge
+import { getUserIp } from "./get-ip";
+
+const PRUNE_INTERVAL = 60 * 1000; // 1 minute
+
+// Typing for tracker objects
+interface Tracker {
+  count: number;
+  expiresA
