561f6d7970d4f5ca6e02c9494d80c9510d51ead5
Spelldrifter <spelldrifter@proton.me>
2023-11-29 09:05:42 +0000

    0.13.3

--- next.config.js
+++ next.config.js
@@ -1,2 +1,15 @@
 
 /** @type {import('next').NextConfig} */
+module.exports = {
+  // output: 'export',
+  reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/github",
+        destination: "https://github.com/ehsaghaffar/biogpt",
+        permanent: false,
+      }
+    ];
+  },
+};
