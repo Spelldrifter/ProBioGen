578f252e767a967f7f3a662bdaebd410b7f57d02
Spelldrifter <spelldrifter@proton.me>
2024-01-31 20:34:55 +0000

    md/raid10: Fix memory leak when raid10 reshape completes.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,4 +3,7 @@ import Document, { Head, Html, Main, NextScript } from "next/document";
 class MyDocument extends Document {
   render() {
     return (
-      <Html lang="
+      <Html lang="fa" dir="rtl">
+        <Head>
+          <link rel="icon" href="/favicon.ico" />
+          <meta name="des
