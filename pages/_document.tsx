5e7bda109e4c8be2237889bb44e9a245e2a1fddd
Spelldrifter <spelldrifter@proton.me>
2024-02-08 20:14:02 +0000

    Remove duplicates.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -20,4 +20,13 @@ class MyDocument extends Document {
             content="ساخت بایو برای شبکه های اجتماعی"
           />
         </Head>
-        <body
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
