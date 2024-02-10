b0801878e84dfabb5c7274e817f74c93529cdbbe
Spelldrifter <spelldrifter@proton.me>
2024-02-10 22:36:52 +0000

    change button color

--- pages/api/openai.ts
+++ pages/api/openai.ts
@@ -1,4 +1,11 @@
 import { OpenAIStream, OpenAIStreamPayload } from "../../libs/OpenAIStream";
 
 if (!process.env.NEXT_PUBLIC_OPENAI_API_KEY) {
-  throw ne
+  throw new Error("Missing env var from OpenAI");
+}
+
+// export const config = {
+//   runtime: "edge",
+// };
+
+const handler = async (req: Request): Promise<a
