5acc084e34dd39a70c04490aad804fe456c9b1ba
Spelldrifter <spelldrifter@proton.me>
2024-04-14 06:35:33 +0000

    latest changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,3 +15,8 @@ module.exports = {
       screens: {
         "2xl": "1400px",
       },
+    },
+    extend: {
+      colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
