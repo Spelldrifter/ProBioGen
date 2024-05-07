8458a1562b1bc1f3206bf71094b129537a51b3ca
Spelldrifter <spelldrifter@proton.me>
2024-05-07 13:19:17 +0000

    change log

--- tailwind.config.js
+++ tailwind.config.js
@@ -58,4 +58,8 @@ module.exports = {
         sm: "calc(var(--radius) - 4px)",
       },
       keyframes: {
-        "ac
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+  
