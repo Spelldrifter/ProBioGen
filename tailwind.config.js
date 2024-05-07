4d7d48e922e75937f4cab39bba2a6fc773f4f028
Spelldrifter <spelldrifter@proton.me>
2024-05-07 08:37:49 +0000

    Fix #27

--- tailwind.config.js
+++ tailwind.config.js
@@ -54,3 +54,8 @@ module.exports = {
       },
       borderRadius: {
         lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "ac
