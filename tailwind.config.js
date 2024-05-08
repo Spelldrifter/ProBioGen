6b0d05873ea5b0f31bcd59c5c6d24831787358ab
Spelldrifter <spelldrifter@proton.me>
2024-05-08 05:58:06 +0000

    netfilter: conntrack: disable generic tracking for known protocols

--- tailwind.config.js
+++ tailwind.config.js
@@ -64,4 +64,9 @@ module.exports = {
         },
         "accordion-up": {
           from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: 
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        
